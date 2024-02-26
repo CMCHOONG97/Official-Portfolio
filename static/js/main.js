f0f9dd745ee5bc8db5e84721f2891f9cea4deca3
CMCHOONG97 <75193355+CMCHOONG97@users.noreply.github.com>
2024-02-26 20:12:30 +0800

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,31 @@
 //This two of "//" is for remark
+/* Navigation Bar Function */
+    function BarFunction(){
+        var menuBtn = document.getElementById("nav-Menu");
+
+        if(menuBtn.className === "navbar-link"){
+            menuBtn.className += "responsive";
+        } else {
+            menuBtn.className = "navbar-link";
+        }
+    }
+
+/* Typing Effect */
+
+
+/* Scroll Reveal Animation */
+
+
+/* Home Page */
+
+
+/* Project Page */
+
+
+/* Awards Page */
+
+
+/* Let's Talk To Me */
+
+
+/* */
